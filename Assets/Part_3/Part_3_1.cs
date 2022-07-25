8afa2ee8c2a90e9c7535ea77c9ab0fd5490a420b
charlielisteminstitutenyc <charlielibx@gmail.com>
2022-07-25 13:35:02 -0400

    Done

--- Assets/Part_3/Part_3_1.cs
+++ Assets/Part_3/Part_3_1.cs
@@ -7,16 +7,25 @@ public class Part_3_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Area(10, 3);       //Result should be 30
-        //Area();            //Result should be 1
-        //Area(2.5f, 3.7f);  //Result should be 9.25f
+        print(Area(10, 3));       //Result should be 30
+        print(Area());            //Result should be 1
+        print(Area(2.5f, 3.7f));  //Result should be 9.25f
     }
 
     //Create a function called Area where you pass in two integer values and
     //return the Area value (1pt)
 
+    public int Area(int a = 1, int b = 1)
+    {
+        return a * b;
+    }
+
     //Add default values to the Area such that width = 1 and length = 1 (1pt)
 
     //Overload the function by creating another Area function but this one takes in
     //float values and returns a float  (1pt) 
+    public float Area(float a, float b)
+    {
+        return a * b;
+    }
 }
--- Assets/Part_3/Part_3_2.cs
+++ Assets/Part_3/Part_3_2.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Part_3_2 : MonoBehaviour
 {
     //Attach this Script to Part_3_Cube
+    Transform hi;
+    Transform mynameis;
+    Transform charlieli;
     void Start()
     {
         //Using PrintOutNameAndPosition print out the name and position of the Part_3_Cube (1pt)
+        hi = GameObject.Find("Part_3_Cube").GetComponent<Transform>();
+        PrintOutNameAndPosition(hi.name, hi.position);
 
         //using GameObject.Find get the transform of Part_3_Sphere and use PrintOutNameAndPosition (1pt)
+        mynameis = GameObject.Find("Part_3_Sphere").GetComponent<Transform>();
+        PrintOutNameAndPosition(mynameis.name, mynameis.position);
 
         //Using transform.Find or transform.Get get the Transform of Part_3_Cylinder which is a child of Part_3_Sphere
         //and use PrintOutNameAndPosition (1pt)
+        charlieli = GameObject.Find("Part_3_Cylinder").GetComponent<Transform>();
+        PrintOutNameAndPosition(charlieli.name, charlieli.position);
 
     }
 
